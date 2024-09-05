b32ef42eeac1c5c2d0398b891d40ee05ad34a07e
Jason Malefakis <anskyny10@aol.com>
2024-09-05 01:36:11 -0400

    add relationships, rules, foreign keys, validation, and RESTful routes

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
+# from flask_sqlalchemy import SQLAlchemy
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -24,6 +25,86 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+class AllRestaurants(Resource):
+    def get(self):
+        restaurants = [restaurant.to_dict(only=('address', 'id', 'name')) for restaurant in Restaurant.query.all()]
+        # restaurants = [restaurant.to_dict() for restaurant in Restaurant.query.all()]
+        return make_response(jsonify(restaurants), 200)
+
+api.add_resource(AllRestaurants, '/restaurants')
+
+# tried the app.route because I couldn't get RESTFUL to work, but then I realized
+# the issue was the serialize_rules...by far the most confusing part for me
+
+# @app.route('/restaurants')
+# def all_restaurants():
+#     restaurants = Restaurant.query.all()
+#     if(not restaurants):
+#         return make_response({"message": "No restaurants found"}, 404)
+#     restaurants_serial = [restaurant.to_dict(only=('address', 'id', 'name')) for restaurant in Restaurant.query.all()]
+#     return make_response(restaurants_serial, 200)
+    
+class IDRestaurant(Resource):
+    
+    def get(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        if not restaurant:
+            return make_response({"error": "Restaurant not found"}, 404)
+        # specific format specified in requirements
+        restaurant_dict = {
+            'id': restaurant.id,
+            'name': restaurant.name,
+            'address': restaurant.address,
+            'restaurant_pizzas': [{
+                'id': rp.id,
+                'pizza': {
+                    'id': rp.pizza.id,
+                    'name': rp.pizza.name,
+                    'ingrdients': rp.pizza.ingredients
+                },
+                'pizza_id': rp.pizza.id,
+                'price': rp.price,
+                'restaurant_id': rp.restaurant_id
+                }
+                # if multiple restaurant_pizzas in the restaurant instance relationship, loop through them
+                for rp in restaurant.rest_pizza
+            ]
+        }
+        return make_response(jsonify(restaurant_dict), 200)
+    
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        if not restaurant:
+            return make_response({"error": "Restaurant not found"}, 404)
+        db.session.delete(restaurant)
+        db.session.commit()
+        return make_response({"message": "Restaurant deleted"}, 204)
+
+api.add_resource(IDRestaurant, '/restaurants/<int:id>')
+
+class AllPizzas(Resource):
+    def get(self):
+        pizzas = [pizza.to_dict(only=('id', 'ingredients', 'name')) for pizza in Pizza.query.all()]
+        return make_response(jsonify(pizzas), 200)
+    
+api.add_resource(AllPizzas, '/pizzas')
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            new_rp = RestaurantPizza(
+                price=data['price'],
+                pizza_id=data['pizza_id'],
+                restaurant_id=data['restaurant_id']
+            )
+            db.session.add(new_rp)
+            db.session.commit()
+            return make_response(jsonify(new_rp.to_dict()), 201)
+        except:
+            return make_response({"errors": ["validation errors"]}, 400)
+        
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -21,8 +21,11 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    rest_pizza = db.relationship('RestaurantPizza', back_populates='restaurant', cascade='all, delete-orphan')
+    pizzas = association_proxy('rest_pizza', 'pizza')
 
     # add serialization rules
+    serialize_rules = ('-rest_pizza.restaurant',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +39,11 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    pizza_rest = db.relationship('RestaurantPizza', back_populates='pizza', cascade='all, delete-orphan')
+    restaurants = association_proxy('pizza_rest', 'restaurant')
 
     # add serialization rules
+    serialize_rules = ('-pizza_rest.pizza',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -48,12 +54,23 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
 
     # add relationships
+    restaurant = db.relationship('Restaurant', back_populates='rest_pizza')
+    pizza = db.relationship('Pizza', back_populates='pizza_rest')
 
     # add serialization rules
+    serialize_rules = ('-restaurant.rest_pizza', '-pizza.pizza_rest')
 
-    # add validation
+    # add validation ("between" means not inclusive by default...the test should be updated)
+    @validates('price')
+    def validate_price(self, key, price):
+        if not 1 <= price <= 30:
+            raise ValueError("Price must be an integer between 1 and 30.")
+        return price
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
